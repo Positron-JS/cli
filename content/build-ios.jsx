26ce8fee2c1d83994465f67ef92d90543f85b955
Akash Kava <39438041+ackava@users.noreply.github.com>
2026-02-05 19:10:06 +0530

    wip

---
+++ content/build-ios.jsx
@@ -0,0 +1,137 @@
+import { invoke, Batch, Run} from "@neurospeech/jex/index.js";
+import { FileSystem } from "@neurospeech/jex/dist/utils/FileSystem.js";
+import { AppleDev } from "@neurospeech/jex/dist/ci/mac/AppleDev.js";
+import { Build } from "@neurospeech/jex/dist/ci/build/Build.js";
+import { XCRun } from "@neurospeech/jex/dist/ci/mac/XCRun.js";
+import assert from "assert";
+import { fileURLToPath } from "url";
+import { join } from "path";
+
+const positronAppDir = fileURLToPath(import.meta.resolve("./app/PositronApp"));
+
+const { default: configs } = await import("../build-ios.config.js");
+
+let applicationDisplayVersion;
+let applicationVersion;
+
+const keyChainPath= "./kc1.db";
+
+let signingIdentity;
+let provisioningProfile;
+
+for (const config of configs) {
+
+    if (!config) {
+        continue;
+    }
+
+    await invoke(<Batch
+        cleanup={<Batch>
+            <AppleDev.Clear
+                keyChainPath={keyChainPath}
+                provisioningProfile={config.provisioningProfileFile}
+                />
+        </Batch>}
+        >
+
+        <FileSystem.RemoveDir
+            path="./build"
+            force={true}
+            recursive={true}
+            />
+
+        <FileSystem.Mkdir
+            path="./build"
+            />
+
+        <FileSystem.CopyFile
+            src="./res/app-icon-background.svg"
+            dest={join(positronAppDir, "Resources/AppIcon/appicon.droid.svg")}
+            />
+
+        <FileSystem.CopyFile
+            src="./res/app-icon-foreground.droid.svg"
+            dest={join(positronAppDir, "Resources/AppIcon/appiconfg.droid.svg")}
+            />
+
+        <FileSystem.CopyFile
+            src="./res/app-icon-foreground.ios.svg"
+            dest={join(positronAppDir, "Resources/AppIcon/appiconfg.ios.svg")}
+            />            
+
+        <FileSystem.MergeJson
+            json={({
+                App: {
+                    Url: config.url
+                }
+            })}
+            path="./maui/PositronApp/appsettings.json"
+            />
+            
+        <Build.PrepareVersion
+            mode={config.buildNumber}
+            then={(x) => (applicationDisplayVersion = `${x.major}.${x.minor}`, applicationVersion = x.build)}
+            />
+
+        { assert(applicationDisplayVersion) }
+        { assert(applicationVersion) }
+
+        <AppleDev.InstallCert
+            certPath={config.certPath}
+            certPass={config.certPass}
+            keyChainPath={keyChainPath}
+            keyChainPass=""
+            provisioningProfile={config.provisioningProfileFile}
+            then={(r) => (signingIdentity = r.friendlyName, provisioningProfile = r.pp.name)}
+            />
+
+        { console.log(`Using identity ${signingIdentity}`)}
+        { console.log(`Using profile ${provisioningProfile}`)}
+
+        {/* <Run
+            cmd="dotnet"
+            args="nuget locals all --clear"
+            /> */}
+
+        <Run
+            cmd="dotnet"
+            timeout={300000}
+            args="workload install maui"
+            />
+
+        <Run
+            cmd="dotnet"
+            timeout={300000}
+            args="workload install android ios"
+            />
+
+        
+        <Run
+            cmd="dotnet"
+            timeout={600000}
+            logData={true}
+            args={["publish",
+                "-f", "net10.0-ios",
+                "-c", "Release",            
+                "/p:BuildIpa=true",
+                "/p:RuntimeIdentifier=ios-arm64",
+                `/p:ApplicationId=${config.id}`,
+                `/p:ApplicationVersion=${applicationVersion}`,
+                `/p:ApplicationDisplayVersion=${applicationDisplayVersion}`,
+                `/p:CodesignKey=${signingIdentity}`,
+                `/p:CodesignProvision=${provisioningProfile}`,
+                "./maui/PositronApp/PositronApp.csproj"]}
+            />
+        
+
+        <XCRun.UploadApp
+            timeout={600000}
+            appPath="./maui/PositronApp/bin/Release/net10.0-ios/ios-arm64/publish/PositronApp.ipa"
+            appType="ios"
+            apiKeyId={config.appStoreConnect.apiKeyId}
+            issuerId={config.appStoreConnect.issuerId}
+            apiPrivateKey={config.appStoreConnect.privateKey}
+            />
+
+    </Batch>);
+}
