3cf865b7ca0390c30c6f23017f191f70f6009aaa
Akash Kava <ackava@gmail.com>
2026-08-11 19:17:43 +0530

    timeout increased

--- content/build-android.jsx
+++ content/build-android.jsx
@@ -68,7 +68,7 @@ for(const config of configs) {
         
         <Run
             cmd="dotnet"
-            timeout={240000}
+            timeout={600000}
             logData={true}
             args={["publish",
                 "-f", config.targetFramework,
@@ -90,6 +90,7 @@ for(const config of configs) {
             />
 
         <PlayStore.Upload
+            timeout={600000}
             packageName={config.id}
             releaseFiles={[`./maui/PositronApp/bin/Release/${config.targetFramework}/publish/*-Signed.aab`]}
             serviceAccountJsonRaw={config.serviceAccountJsonRaw}
